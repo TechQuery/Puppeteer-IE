4894ecab5a6f3ef6490703e46147412b6b5cae1a
demongodYY <yyuuqqii@outlook.com>
2018-04-08 21:59:37 +0800

    init keyboard interface

---
+++ source/Keyboard.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class Keyboard {
+    /**
+     * @constructs
+     *
+     * @param {Page} page - The page which this keyboard press
+     */
+    constructor(page) {
+        this._page = page;
+    }
+
+    async down(key, options) {
+
+    }
+    
+    async press(key, options) {
+
+    }
+
+    async sendCharacter(char) {
+
+    }
+
+    async type(text, options) {
+
+    }
+    async up(key) {
+
+    }
+}
